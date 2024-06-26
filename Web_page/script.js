6f9387cc1ae3bbf36ea8548453fd412091190af3
yahya chami <forcontact.we@gmail.com>
2024-06-26 14:22:51 +0100

    Latest Update

---
+++ Web_page/script.js
@@ -0,0 +1,10 @@
+function doThing() {
+    document.getElementById('page1').style.transform = 'translateX(-100vw)';
+    document.getElementById('page2').style.transform = 'translateX(0)';
+}
+
+// script.js
+
+document.getElementById('arrow').addEventListener('click', function() {
+    
+});
