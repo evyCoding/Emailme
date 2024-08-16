349aa8dadc1978b3a46c7a32813e7b02290afbf9
yahya chami <123872563+evyCoding@users.noreply.github.com>
2024-08-16 23:20:51 +0100

    Update sendMail.py

--- sendMail.py
+++ sendMail.py
@@ -5,12 +5,12 @@ from writeAndReadMethods import *
 
 smtp_server = 'smtp.gmail.com'
 smtp_port = 587
-from_email = 'emailme.po@gmail.com'
-password = 'tzep izyb nvhi ryla '
+from_email = 'test@gmail.com'
+password = 'google password'
 
 load_dotenv()
 MONGODB_URI = os.environ.get(
-    'mongodb://127.0.0.1:27017/mongo?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.6')
+    'link to the database')
 client = MongoClient(MONGODB_URI)
 db = client['Emailme']
 cursor = db['infos']
